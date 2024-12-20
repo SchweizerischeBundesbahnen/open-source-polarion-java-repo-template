98c7f0f26f6acd0b5d33cd229b4d685e0c5baa52
ariwk <adam.ruberti@sbb.ch>
2024-12-20 14:12:04 +0100

    chore: add info into openapi.json

---
+++ src/main/java/ch/sbb/polarion/extension/extension_name/rest/controller/OpenAPIInfo.java
@@ -0,0 +1,17 @@
+package ch.sbb.polarion.extension.extension_name.rest.controller;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "extension-name REST API",
+                version = "v1",
+                description = "",
+                termsOfService = "",
+                contact = @Contact(name = "", url = ""),
+                license = @License(name = "", url = "")))
+public class OpenAPIInfo {
+}
